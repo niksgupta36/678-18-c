38d892c0479b18b441b9fbe6da105cea87e752ed
ashima1491 <sharma.ashima91@gmail.com>
2018-09-22 17:53:02 -0500

    application set up command working

---
+++ aggiestack_project/__main__.py
@@ -0,0 +1,9 @@
+
+
+def main():
+   
+    print("hii")
+   
+if __name__ == '__main__':
+    main()
+    
---
+++ aggiestack_project/config.py
@@ -0,0 +1,8 @@
+'''
+Created on Sep 22, 2018
+
+@author: sharm
+'''
+
+class Config():
+    print("hello")
