11717424467a1b83ed63626b5b94a32591adf106
Nikhil Gupta <niksgupta36@gmail.com>
2018-09-22 22:04:48 -0500

    DB connection added

--- aggiestack_project/utility/database_connection.py
+++ aggiestack_project/utility/database_connection.py
@@ -13,14 +13,14 @@ import os
 
 
 def connectMongo():
-    data = {}
-    #print (os.getcwd())
-    if(os.path.isfile('./connect_db.json')):
-        data = json.load(open('./connect_db.json'))
+#     data = {}
+#     print (os.getcwd())
+#     if(os.path.isfile('./connect_db.json')):
+#         data = json.load(open('./connect_db.json'))
 
-    if ('HOST' in data) and ('DB' in data):
-        HOST = data['HOST']
-        DB = data['DB']
+#     if ('HOST' in data) and ('DB' in data):
+#         HOST = data['HOST']
+#         DB = data['DB']
 #     else:
 #         data['HOST'] = raw_input('Enter the host URL: ')
 #         data['DB']   = raw_input('Enter the database to connect: ')
@@ -31,8 +31,10 @@ def connectMongo():
         
             
 
-    url = "mongodb://" + HOST + "/" + DB
+    url = 'mongodb://admin:admin@cluster0-shard-00-00-v0u4h.mongodb.net:27017,cluster0-shard-00-01-v0u4h.mongodb.net:27017,cluster0-shard-00-02-v0u4h.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+    
+    
     client = pymongo.MongoClient(url)
-    db = client[DB]
+    db = client.test
     #collection = db[chatroom]
     return db
