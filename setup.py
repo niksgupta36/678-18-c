0adada557afbb69fea7ee04ce4a2921ea7261c96
ashima1491 <sharma.ashima91@gmail.com>
2018-09-24 13:54:06 -0500

    version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name = 'aggiestack',
-    version = '0.1.0',
+    version = '0.1',
     packages = ['aggiestack_project','aggiestack_project.commands','aggiestack_project.utility'],
     entry_points = {
         'console_scripts': [
