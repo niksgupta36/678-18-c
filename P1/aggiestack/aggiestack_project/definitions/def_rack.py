a47fb9448d2fdd80550748d5b616dcb8b72ca12e
ashima1491 <sharma.ashima91@gmail.com>
2018-11-11 21:56:18 -0600

    count_rack

--- P1/aggiestack/aggiestack_project/definitions/def_rack.py
+++ P1/aggiestack/aggiestack_project/definitions/def_rack.py
@@ -4,5 +4,6 @@ import aggiestack_project.database.database_crud as db_crud
 
 def getRacks():
     print ("***** Racks available on the server *****")
+    print(db_crud.getCount('rack_collection'))
     db_crud.getRecords('rack_collection',["rack_name","rack_storage"])
             
