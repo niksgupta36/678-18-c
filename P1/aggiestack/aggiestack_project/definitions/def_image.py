7f767329c6d6767009349e10c5ecfd2953cec455
Nikhil Gupta <niksgupta36@gmail.com>
2018-11-11 21:42:03 -0600

    added config-images functionality

--- P1/aggiestack/aggiestack_project/definitions/def_image.py
+++ P1/aggiestack/aggiestack_project/definitions/def_image.py
@@ -1,7 +1,7 @@
 import aggiestack_project.database.database_crud as db_crud
 
 def insertImages(images):
-    db_crud.insertRecords(images,["image_name","path"],'image_collection', 'image_name',0)
+    db_crud.insertRecords(images,["image_name","image_size","path"],'image_collection', 'image_name',0)
 
 def getImages():
 
